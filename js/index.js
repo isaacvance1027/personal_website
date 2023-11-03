38624ff14e51ef54ef52dd4e091ee5d8b2bae853
Bryan Mills <bryan.mills@palmetto.lan>
2023-11-03 16:15:27 -0600

    Added dynamic hexagons to index page

--- js/index.js
+++ js/index.js
@@ -19,3 +19,28 @@ document.addEventListener("DOMContentLoaded", function () {
         }, 250);
     });
 });
+
+window.addEventListener('resize', () => {
+    const width = window.innerWidth;
+    const hexagons = document.querySelectorAll('.hex');
+
+    const hexagonBeforeStyle = document.getElementById('hexagon-before');
+    const hexagonAfterStyle = document.getElementById('hexagon-after');
+
+    if (width < 1280) {
+        hexagons.forEach(hexagon => {
+            const hexagonWidth = width / 12.3;
+            const hexagonHeight = hexagonWidth * 0.62;
+
+            hexagon.style.width = `${hexagonWidth}px`;
+            hexagon.style.height = `${hexagonHeight}px`;
+
+            hexagonBeforeStyle.innerHTML = `.hex:before{content:"";width:0;height:0;border-bottom:${hexagonWidth * 0.3}px solid;border-color:inherit;border-left:${hexagonWidth * 0.5}px solid transparent;border-right:${hexagonWidth * 0.5}px solid transparent;position:absolute;top:-${hexagonWidth * 0.3}px;right:0px;}`
+            hexagonAfterStyle.innerHTML = `.hex:after{content:"";width:0;position:absolute;right:0px;bottom:-${hexagonWidth * 0.3}px;border-top:${hexagonWidth * 0.3}px solid;border-color:inherit;border-left: ${hexagonWidth * 0.5}px solid transparent;border-right: ${hexagonWidth * 0.5}px solid transparent;}`
+        });
+
+        
+    }
+});
+
+
