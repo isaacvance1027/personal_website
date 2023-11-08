6e20923d21e83dcf1d6f334c587e788f00a2bbe9
Bryan Mills <bryan.mills@palmetto.lan>
2023-11-08 16:02:22 -0700

    Functional shrinking hexes

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 // Custom scripts for Index Page
 
 document.addEventListener("DOMContentLoaded", function () {
+
     const header = document.querySelector(".header");
 
     async function activeAdd() {
@@ -20,27 +21,30 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-window.addEventListener('resize', () => {
+// Select elements outside the event handler
+const hexagons = document.querySelectorAll('.hex');
+const hexagonBeforeStyle = document.getElementById('hexagon-before');
+const hexagonAfterStyle = document.getElementById('hexagon-after');
+
+function resizeHexagons() {
     const width = window.innerWidth;
-    const hexagons = document.querySelectorAll('.hex');
 
-    const hexagonBeforeStyle = document.getElementById('hexagon-before');
-    const hexagonAfterStyle = document.getElementById('hexagon-after');
+    hexagons.forEach(hexagon => {
+        let hexagonWidth = width / 12.3;
+        let hexagonHeight = hexagonWidth * 0.7;
 
-    if (width < 1280) {
-        hexagons.forEach(hexagon => {
-            const hexagonWidth = width / 12.3;
-            const hexagonHeight = hexagonWidth * 0.62;
+        // Round down the dimensions
+        hexagonWidth = Math.floor(hexagonWidth);
+        hexagonHeight = Math.floor(hexagonHeight);
 
-            hexagon.style.width = `${hexagonWidth}px`;
-            hexagon.style.height = `${hexagonHeight}px`;
+        hexagon.style.width = `${hexagonWidth}px`;
+        hexagon.style.height = `${hexagonHeight}px`;
 
-            hexagonBeforeStyle.innerHTML = `.hex:before{content:"";width:0;height:0;border-bottom:${hexagonWidth * 0.3}px solid;border-color:inherit;border-left:${hexagonWidth * 0.5}px solid transparent;border-right:${hexagonWidth * 0.5}px solid transparent;position:absolute;top:-${hexagonWidth * 0.3}px;right:0px;}`
-            hexagonAfterStyle.innerHTML = `.hex:after{content:"";width:0;position:absolute;right:0px;bottom:-${hexagonWidth * 0.3}px;border-top:${hexagonWidth * 0.3}px solid;border-color:inherit;border-left: ${hexagonWidth * 0.5}px solid transparent;border-right: ${hexagonWidth * 0.5}px solid transparent;}`
-        });
+        hexagonBeforeStyle.innerHTML = `.hex:before{content:"";width:0;height:0;border-bottom:${Math.floor(hexagonWidth * 0.3)}px solid;border-color:inherit;border-left:${Math.floor(hexagonWidth * 0.5)}px solid transparent;border-right:${Math.floor(hexagonWidth * 0.5)}px solid transparent;position:absolute;top:-${Math.floor(hexagonWidth * 0.3)}px;right:0;}`;
+        hexagonAfterStyle.innerHTML = `.hex:after{content:"";width:0;position:absolute;right:0;bottom:-${Math.floor(hexagonWidth * 0.3)}px;border-top:${Math.floor(hexagonWidth * 0.3)}px solid;border-color:inherit;border-left:${Math.floor(hexagonWidth * 0.5)}px solid transparent;border-right:${Math.floor(hexagonWidth * 0.5)}px solid transparent;}`;
+    });
+}
 
-        
-    }
-});
-
-
+// Call the function initially and on window resize
+resizeHexagons();
+window.addEventListener('resize', resizeHexagons);
